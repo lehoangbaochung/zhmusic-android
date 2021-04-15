f3661c888b3d5b1f13076bfff3b2a0e32f775735
Le Hoang Bao Chung <1851061805@e.tlu.edu.vn>
2021-04-15 18:38:38 +0700

    15/04/2021

---
+++ app/src/main/java/com/zitherharp/zhmusic/ui/fragment/PlaylistFragment.java
@@ -0,0 +1,18 @@
+package com.zitherharp.zhmusic.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.zitherharp.zhmusic.R;
+
+public class PlaylistFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_playlist, container, false);
+        return root;
+    }
+}
